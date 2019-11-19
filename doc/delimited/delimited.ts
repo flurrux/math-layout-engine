2dde6171f1e3198c099e8f76aa01544180989652
Christian Hoffmann <christianhoffmann@outlook.com>
2019-11-19 18:49:15 +0100

    added little bit of documentation to delimiter nodes

--- doc/delimited/delimited.ts
+++ doc/delimited/delimited.ts
@@ -19,8 +19,15 @@ to do that, use DelimitedNode.
 
 these are all the possible delimiter-characters that can be used:  
 
-"(", ")", "[", "]", "{", "}", "⟨", "⟩", "|", "⌈", "⌉", "⌊", "⌋"
+"(" ")" 
+"[" "]" 
+"{" "}" 
+"⟨" "⟩" 
+"⌈" "⌉" 
+"⌊" "⌋"
+"|" 
 
-
+note that delimiters are centered on the axis (0.25) and sized in such a way to completely cover the enclosed nodes.  
+if a node has much more height than depth, the lower part of the delimiter will be empty.  
 
 `;
