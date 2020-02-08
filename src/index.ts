945832ca0b4cdf547d79bdc792857ec1d149113f
flurrux <christianhoffmann@outlook.com>
2020-02-08 19:34:18 +0100

    - exposed parse-function in index - incremented minor version

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { layout as layoutFormula } from './layout/layout';
 export { centerNodeOnCanvas, loadKatexFontFaces, renderFormulaLayout } from './rendering/render';
+export { parse as stringToFormula } from './parsing/parser';
