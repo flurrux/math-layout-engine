4186cf82e26da60772772171af2b3d58860e437c
flurrux <christianhoffmann@outlook.com>
2020-08-18 15:14:11 +0200

    added local fonts to index.ts and added /dist to repository

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { layout as layoutFormula } from './layout/layout';
-export { centerNodeOnCanvas, loadKatexFontFaces, renderFormulaLayout } from './rendering/render';
+export { centerNodeOnCanvas, renderFormulaLayout } from './rendering/render';
+export { loadFontFaces } from './fonts/load-fonts';
 export { parse as stringToFormula } from './parsing/parser';
